aac50ec49743c4d98ddb380e337976388ff336e1
GustavoCesarSantos <gustavocs789@gmail.com>
2021-06-24 23:11:39 -0300

    Feito arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,15 @@ const { Router } = require('express');
 const router = Router();
 
 const createCarController = require('./components/Car/CreateCar/CreateCarController');
+const listCarsController = require('./components/Car/ListCars/ListCarsController');
+const listCarController = require('./components/Car/ListCar/ListCarController');
+const updateCarController = require('./components/Car/UpdateCar/UpdateCarController');
+const deleteCarController = require('./components/Car/DeleteCar/DeleteCarController');
 
 router.post('/cars', createCarController.handle);
+router.get('/cars', listCarsController.handle);
+router.get('/cars/:carId', listCarController.handle);
+router.put('/cars/:carId', updateCarController.handle);
+router.delete('/cars/:carId', deleteCarController.handle);
 
 module.exports = { router };
